2e6adee29be714942492f9a14acb8733d6bd43a9
zhyperflowtzy <lordlawliet1100@gmail.com>
2024-10-20 20:50:58 +0700

    Create indexx.php

---
+++ indexx.php
@@ -0,0 +1,23 @@
+<?php
+// PHP permanent URL redirection
+header("Location: https://www.baduycorner.com/", true, 301);
+exit();
+?>
+
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';  
