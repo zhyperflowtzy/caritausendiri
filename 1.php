d959dd5894ba8301f751018633d138a4367aaa7c
zhyperflowtzy <lordlawliet1100@gmail.com>
2024-10-22 22:12:19 +0700

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,48 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Zhyper</title>
+</head>
+<body>
+    <p style="text-align:-webkit-center">
+        <span style="font-size:medium">
+            <span style="color:#000000">
+                <span style="font-family:Courier">
+                    <span style="font-style:normal">
+                        <span style="font-variant-ligatures:normal">
+                            <span style="font-weight:400">
+                                <span style="white-space:normal">
+                                    <span style="background-color:#ffffff">
+                                        <span style="text-decoration-thickness:initial">
+                                            <span style="text-decoration-style:initial">
+                                                <span style="text-decoration-color:initial">
+                                                    <font size="7px">
+                                                        <b><img alt="" src="https://media1.tenor.com/m/hQyTyr_AvKwAAAAC/troll-lel.gif" /><br />Zhyper</b>
+                                                    </font><br />
+                                                    <font size="5px">Editor Learn Seo</font><br/>
+                                                    <font size="5px">
+                                                        <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="6a090b18051f190f052a051f1e0605050144090507">contact me Zhyper Seo</a>
+                                                    </font>
+                                                </span>
+                                            </span>
+                                        </span>
+                                    </span>
+                                </span>
+                            </span>
+                        </span>
+                    </span>
+                </span>
+            </span>
+        </span>
+    </p>
+    <p><audio src="https://l.top4top.io/m_3116whudo1.mp3" id="alertSound" loop=""></audio></p>
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script></body>
+<script>
+    document.body.addEventListener('click', function() {
+        var audio = document.getElementById('alertSound');
+        audio.play();
+    }, { once: true });
+</script>
+</html>
